6b89e9914a49bcb49c0f097d74f13faf0af09477
Jimil Patel <jimilkumarpatel21@gnu.ac.in>
2023-08-13 21:03:27 +0530

    Practical-2 done

---
+++ app/src/main/java/com/example/mad_practical_2_21012011084/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.mad_practical_2_21012011084
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    val TAG="MainActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        showMessage("onCreate Method is called")
+    }
+    fun showMessage(msg:String){
+        Log.i(TAG, msg)
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        val v = findViewById<View>(R.id.myCoordinatorLayout)
+        if(v!=null){
+            Snackbar.make(
+                v,msg,
+                Snackbar.LENGTH_SHORT
+            ).show()
+        }
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        showMessage("onStart method is called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        showMessage("onResume method is called")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        showMessage("onPause method is called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        showMessage("onStop method is called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        showMessage("onDestroy method is called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        showMessage("onRestart method is called")
+    }
+}
